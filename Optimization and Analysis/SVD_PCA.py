5e8a0cfedcc6fab9ac5b758b6c6ac48ccd942d4c
Kunal Sharma <kushkunal077@gmail.com>
2021-08-14 23:10:21 +0530

    Create SVD_PCA.py

---
+++ Optimization and Analysis/SVD_PCA.py
@@ -0,0 +1,75 @@
+from numpy import array
+from numpy import diag
+from numpy import zeros
+from scipy.linalg import svd
+# define a matrix
+A = array([
+	[1,2,3,4,5,6,7,8,9,10],
+	[11,12,13,14,15,16,17,18,19,20],
+	[21,22,23,24,25,26,27,28,29,30]])
+print(A)
+# Singular-value decomposition
+U, s, VT = svd(A)
+# create m x n Sigma matrix
+Sigma = zeros((A.shape[0], A.shape[1]))
+# populate Sigma with n x n diagonal matrix
+Sigma[:A.shape[0], :A.shape[0]] = diag(s)
+# select
+n_elements = 2
+Sigma = Sigma[:, :n_elements]
+VT = VT[:n_elements, :]
+# reconstruct
+B = U.dot(Sigma.dot(VT))
+print(B)
+# transform
+T = U.dot(Sigma)
+print(T)
+T = A.dot(VT.T)
+print(T)
+
+# Pseudoinverse via SVD
+from numpy import array
+from numpy.linalg import svd
+from numpy import zeros
+from numpy import diag
+# define matrix
+A = array([
+	[0.1, 0.2],
+	[0.3, 0.4],
+	[0.5, 0.6],
+	[0.7, 0.8]])
+print(A)
+# calculate svd
+U, s, VT = svd(A)
+# reciprocals of s
+d = 1.0 / s
+# create m x n D matrix
+D = zeros(A.shape)
+# populate D with n x n diagonal matrix
+D[:A.shape[1], :A.shape[1]] = diag(d)
+# calculate pseudoinverse
+B = VT.T.dot(D.T).dot(U.T)
+print(B)
+
+# grid search shrinkage for lda
+from numpy import arange
+from sklearn.datasets import make_classification
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+# define dataset
+X, y = make_classification(n_samples=1000, n_features=10, n_informative=10, n_redundant=0, random_state=1)
+# define model
+model = LinearDiscriminantAnalysis(solver='lsqr')
+# define model evaluation method
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+# define grid
+grid = dict()
+grid['shrinkage'] = arange(0, 1, 0.01)
+# define search
+search = GridSearchCV(model, grid, scoring='accuracy', cv=cv, n_jobs=-1)
+# perform the search
+results = search.fit(X, y)
+# summarize
+print('Mean Accuracy: %.3f' % results.best_score_)
+print('Config: %s' % results.best_params_)
