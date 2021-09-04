becbcdeb6e0ada488e79f3b68264a0fb06eb0177
Kunal Sharma <kushkunal077@gmail.com>
2021-09-04 13:10:23 +0530

    Update SVD_PCA.py

--- Optimization and Analysis/SVD_PCA.py
+++ Optimization and Analysis/SVD_PCA.py
@@ -2,7 +2,9 @@ from numpy import array
 from numpy import diag
 from numpy import zeros
 from scipy.linalg import svd
+
 # define a matrix
+
 A = array([
 	[1,2,3,4,5,6,7,8,9,10],
 	[11,12,13,14,15,16,17,18,19,20],
